b1808656080a28d1979cf7b922b04efe6075430d
Devon Parsons <Noved90@yahoo.com>
2019-01-30 08:29:34 -0500

    Creation
    Start v0.1

--- SimpleCalcuator/Program.cs
+++ SimpleCalcuator/Program.cs
@@ -8,8 +8,68 @@ namespace SimpleCalcuator
 {
     class Program
     {
+        public static string modCharactor = "%";
+        public static string additionChar = "+";
+        public static string subtractionChar = "-";
+        public static string divisionChar = "/";
+        public static string multiplicationChar = "*";
+        public static float CalculateAnswer = 0;
+        public static string UserOperand = "";
+        public static float UserNumInput = 0;
+        public static float UserNumInputTwo = 0;
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("Enter First Num");
+            float.TryParse(Console.ReadLine(), out UserNumInput);
+            Console.WriteLine("Enter Second Num");
+            float.TryParse(Console.ReadLine(), out UserNumInputTwo);
+            Console.WriteLine("Enter Charactor: +,-,/,*,1 : ");
+            UserOperand = Console.ReadLine();
+            if (UserOperand == modCharactor)
+            {
+                CalculateAnswer = UserNumInput % UserNumInputTwo;
+                Console.WriteLine(CalculateAnswer.ToString());
+                Console.ReadKey();
+            }
+            else if (UserOperand == additionChar)
+            {
+                CalculateAnswer = UserNumInput + UserNumInputTwo;
+                Console.WriteLine(CalculateAnswer.ToString());
+                Console.ReadKey();
+            }
+            else if(UserOperand == subtractionChar)
+            {
+                CalculateAnswer = UserNumInput - UserNumInputTwo;
+                Console.WriteLine(CalculateAnswer.ToString());
+                Console.ReadKey();
+            }
+            else if(UserOperand == additionChar)
+            {
+                CalculateAnswer = UserNumInput + UserNumInputTwo;
+                Console.WriteLine(CalculateAnswer.ToString());
+            }
+            else if(UserOperand == multiplicationChar)
+            {
+                CalculateAnswer = UserNumInput * UserNumInputTwo;
+                Console.WriteLine(CalculateAnswer.ToString());
+                Console.ReadKey();
+            }
+            else if(UserOperand == divisionChar)
+            {
+                CalculateAnswer = UserNumInput / UserNumInputTwo;
+                Console.WriteLine(CalculateAnswer.ToString());
+                Console.ReadKey();
+            }
+
+            else
+            {
+                Console.WriteLine("Nope");
+                return;
+
+            }
+            
+                
         }
     }
 }
